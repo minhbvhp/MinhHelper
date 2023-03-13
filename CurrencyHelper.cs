f836bc6efea0419f58d1237fe6ea1b79f237a087
trankhoaminh <tranminhbvhp@gmail.com>
2023-03-13 17:05:53 +0700

    add Currency helper

--- CurrencyHelper.cs
+++ CurrencyHelper.cs
@@ -5,6 +5,106 @@ namespace MinhHelper
 {
     public class CurrencyHelper
     {
+        public static string NumberToWords(decimal inputNumber)
+        {
+            var number = SplitDecimalPoint(inputNumber);
+            string firstPart = LongToTextVN(number.Item1);
+            string secondPart = LongToTextVN(number.Item2);
+            return firstPart + " và " + secondPart;
+        }
+        private static Tuple<long, long> SplitDecimalPoint(decimal inputNumber)
+        {
+            string s = inputNumber.ToString("0.00", CultureInfo.InvariantCulture);
+            string[] parts = s.Split('.');
+            var beforeDecimal = long.Parse(parts[0]);
+            var afterDecimal = long.Parse(parts[1]);
+            var result = Tuple.Create(beforeDecimal, afterDecimal);
+            return result;
+        }
+        private static string LongToTextVN(long inputNumber)
+        {
+            string[] unitNumbers = new string[] { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+            string[] placeValues = new string[] { "", "nghìn", "triệu", "tỷ" };
+            bool isNegative = false;
+
+            var sNumber = inputNumber.ToString();
+            var number = inputNumber;
+            if (number < 0)
+            {
+                number = -number;
+                sNumber = number.ToString();
+                isNegative = true;
+            }
+
+            int ones, tens, hundreds;
+
+            int positionDigit = sNumber.Length;
+
+            string result = " ";
+
+            if (positionDigit == 0)
+                result = unitNumbers[0] + result;
+            else
+            {
+                // 0:       ###
+                // 1: nghìn ###,###
+                // 2: triệu ###,###,###
+                // 3: tỷ    ###,###,###,###
+                int placeValue = 0;
+
+                while (positionDigit > 0)
+                {
+                    // Check last 3 digits remain ### (hundreds tens ones)
+                    tens = hundreds = -1;
+                    ones = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                    positionDigit--;
+                    if (positionDigit > 0)
+                    {
+                        tens = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                        positionDigit--;
+                        if (positionDigit > 0)
+                        {
+                            hundreds = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                            positionDigit--;
+                        }
+                    }
+
+                    if ((ones > 0) || (tens > 0) || (hundreds > 0) || (placeValue == 3))
+                        result = placeValues[placeValue] + result;
+
+                    placeValue++;
+                    if (placeValue > 3) placeValue = 1;
+
+                    if ((ones == 1) && (tens > 1))
+                        result = "một " + result;
+                    else
+                    {
+                        if ((ones == 5) && (tens > 0))
+                            result = "lăm " + result;
+                        else if (ones > 0)
+                            result = unitNumbers[ones] + " " + result;
+                    }
+                    if (tens < 0)
+                        break;
+                    else
+                    {
+                        if ((tens == 0) && (ones > 0)) result = "lẻ " + result;
+                        if (tens == 1) result = "mười " + result;
+                        if (tens > 1) result = unitNumbers[tens] + " mươi " + result;
+                    }
+                    if (hundreds < 0) break;
+                    else
+                    {
+                        if ((hundreds > 0) || (tens > 0) || (ones > 0))
+                            result = unitNumbers[hundreds] + " trăm " + result;
+                    }
+                    result = " " + result;
+                }
+            }
+            result = result.Trim();
+            if (isNegative) result = "âm " + result;
+            return result;
+        }
 
     }
 }
