9909dd384dcb3ef414227e1d39cdef5a4aacefc7
trankhoaminh <tranminhbvhp@gmail.com>
2023-03-13 18:30:19 +0700

    change to ChuyenSo method and add remove whitespaces extension method

--- CurrencyHelper.cs
+++ CurrencyHelper.cs
@@ -5,13 +5,28 @@ namespace MinhHelper
 {
     public class CurrencyHelper
     {
-        public static string NumberToWords(decimal inputNumber)
+        public static string NumberToWords(decimal inputNumber, NationalCurrency nationalCurrency)
         {
             var number = SplitDecimalPoint(inputNumber);
-            string firstPart = LongToTextVN(number.Item1);
-            string secondPart = LongToTextVN(number.Item2);
-            return firstPart + " và " + secondPart;
-        }
+            var currencyUnit = new CurrencyUnit(nationalCurrency);
+
+            string firstPart = ChuyenSo(number.Item1);
+            string secondPart = ChuyenSo(number.Item2);
+            string result = "";
+
+            if (String.IsNullOrEmpty(secondPart) || String.IsNullOrWhiteSpace(secondPart) || secondPart == "không")
+            {
+                result = firstPart + currencyUnit.Unit + currencyUnit.Suffix;
+            }
+            else
+            {
+                result = firstPart + currencyUnit.Unit + " và " + secondPart + currencyUnit.Subsidiary + currencyUnit.Suffix;
+            }
+             
+
+            return result.RemoveInPlaceCharArray();
+        }        
+
         private static Tuple<long, long> SplitDecimalPoint(decimal inputNumber)
         {
             string s = inputNumber.ToString("0.00", CultureInfo.InvariantCulture);
@@ -21,89 +36,115 @@ namespace MinhHelper
             var result = Tuple.Create(beforeDecimal, afterDecimal);
             return result;
         }
-        private static string LongToTextVN(long inputNumber)
+        
+        private static string ChuyenSo(long inputNumber)
         {
-            string[] unitNumbers = new string[] { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
-            string[] placeValues = new string[] { "", "nghìn", "triệu", "tỷ" };
-            bool isNegative = false;
+            string number = inputNumber.ToString();
+            string[] dv = { "", "mươi", "trăm", "nghìn", "triệu", "tỉ" };
+            string[] cs = { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+            string doc;
+            int i, j, k, n, len, found, ddv, rd;
 
-            var sNumber = inputNumber.ToString();
-            var number = inputNumber;
-            if (number < 0)
+            len = number.Length;
+            number += "ss";
+            doc = "";
+            found = 0;
+            ddv = 0;
+            rd = 0;
+
+            i = 0;
+            while (i < len)
             {
-                number = -number;
-                sNumber = number.ToString();
-                isNegative = true;
-            }
+                //So chu so o hang dang duyet
+                n = (len - i + 2) % 3 + 1;
 
-            int ones, tens, hundreds;
-
-            int positionDigit = sNumber.Length;
-
-            string result = " ";
-
-            if (positionDigit == 0)
-                result = unitNumbers[0] + result;
-            else
-            {
-                // 0:       ###
-                // 1: nghìn ###,###
-                // 2: triệu ###,###,###
-                // 3: tỷ    ###,###,###,###
-                int placeValue = 0;
-
-                while (positionDigit > 0)
+                //Kiem tra so 0
+                found = 0;
+                for (j = 0; j < n; j++)
                 {
-                    // Check last 3 digits remain ### (hundreds tens ones)
-                    tens = hundreds = -1;
-                    ones = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
-                    positionDigit--;
-                    if (positionDigit > 0)
+                    if (number[i + j] != '0')
                     {
-                        tens = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
-                        positionDigit--;
-                        if (positionDigit > 0)
+                        found = 1;
+                        break;
+                    }
+                }
+
+                //Duyet n chu so
+                if (found == 1)
+                {
+                    rd = 1;
+                    for (j = 0; j < n; j++)
+                    {
+                        ddv = 1;
+                        switch (number[i + j])
                         {
-                            hundreds = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
-                            positionDigit--;
+                            case '0':
+                                if (n - j == 3) doc += cs[0] + " ";
+                                if (n - j == 2)
+                                {
+                                    if (number[i + j + 1] != '0') doc += "lẻ ";
+                                    ddv = 0;
+                                }
+                                break;
+                            case '1':
+                                if (n - j == 3) doc += cs[1] + " ";
+                                if (n - j == 2)
+                                {
+                                    doc += "mười ";
+                                    ddv = 0;
+                                }
+                                if (n - j == 1)
+                                {
+                                    if (i + j == 0) k = 0;
+                                    else k = i + j - 1;
+
+                                    if (number[k] != '1' && number[k] != '0')
+                                        doc += "mốt ";
+                                    else
+                                        doc += cs[1] + " ";
+                                }
+                                break;
+                            case '5':
+                                if (i + j == len - 1)
+                                    doc += "lăm ";
+                                else
+                                    doc += cs[5] + " ";
+                                break;
+                            default:
+                                doc += cs[(int)number[i + j] - 48] + " ";
+                                break;
+                        }
+
+                        //Doc don vi nho
+                        if (ddv == 1)
+                        {
+                            doc += dv[n - j - 1] + " ";
                         }
                     }
-
-                    if ((ones > 0) || (tens > 0) || (hundreds > 0) || (placeValue == 3))
-                        result = placeValues[placeValue] + result;
-
-                    placeValue++;
-                    if (placeValue > 3) placeValue = 1;
-
-                    if ((ones == 1) && (tens > 1))
-                        result = "một " + result;
-                    else
-                    {
-                        if ((ones == 5) && (tens > 0))
-                            result = "lăm " + result;
-                        else if (ones > 0)
-                            result = unitNumbers[ones] + " " + result;
-                    }
-                    if (tens < 0)
-                        break;
-                    else
-                    {
-                        if ((tens == 0) && (ones > 0)) result = "lẻ " + result;
-                        if (tens == 1) result = "mười " + result;
-                        if (tens > 1) result = unitNumbers[tens] + " mươi " + result;
-                    }
-                    if (hundreds < 0) break;
-                    else
-                    {
-                        if ((hundreds > 0) || (tens > 0) || (ones > 0))
-                            result = unitNumbers[hundreds] + " trăm " + result;
-                    }
-                    result = " " + result;
                 }
+
+
+                //Doc don vi lon
+                if (len - i - n > 0)
+                {
+                    if ((len - i - n) % 9 == 0)
+                    {
+                        if (rd == 1)
+                            for (k = 0; k < (len - i - n) / 9; k++)
+                                doc += "tỉ ";
+                        rd = 0;
+                    }
+                    else
+                        if (found != 0) doc += dv[((len - i - n + 1) % 9) / 3 + 2] + " ";
+                }
+
+                i += n;
             }
-            result = result.Trim();
-            if (isNegative) result = "âm " + result;
-            return result;
+
+            if (len == 1)
+                if (number[0] == '0' || number[0] == '5') return cs[(int)number[0] - 48];
+
+            return doc;
         }
 
     }
---
+++ CurrencyUnit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhHelper
+{
+    internal class CurrencyUnit
+    {
+
+        NationalCurrency nationalCurrency;
+        public CurrencyUnit(NationalCurrency nationalCurrency)
+        {
+            this.nationalCurrency = nationalCurrency;
+        }
+        public string Unit
+        {
+            get
+            {
+                switch (nationalCurrency)
+                {
+                    case NationalCurrency.USD:
+                        return " Đô la ";
+
+                    default:
+                        return " đồng ";
+                }
+            }
+        }
+
+        public string Subsidiary
+        {
+            get
+            {
+                switch (nationalCurrency)
+                {
+                    case NationalCurrency.USD:
+                        return " xu ";
+
+                    default:
+                        return " hào ";
+                }
+            }
+        }
+
+        public string Suffix
+        {
+            get
+            {
+                switch (nationalCurrency)
+                {
+                    case NationalCurrency.USD:
+                        return " Mỹ";
+
+                    default:
+                        return "";
+                }
+            }
+        }
+    }
+}
